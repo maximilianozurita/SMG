310e18cfeff0257628f1db5404398eb3e97685d8
Santiago Maldonado <81437728+santiago010796@users.noreply.github.com>
2021-11-04 05:10:02 -0300

    Clases "Categoria" y "CategoriaNegocio"

---
+++ Proyecto/TPCuatrimestral_Cordoba_Zurita_Maldonado/eCommerce videojuegos/CategoriaNegocio.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+
+namespace eCommerce_videojuegos
+{
+    public class CategoriaNegocio
+    {
+        public List<Categoria> listar()
+        {
+            List<Categoria> lista = new List<Categoria>();
+            SqlConnection conexion = new SqlConnection();
+            SqlCommand comando = new SqlCommand();
+            SqlDataReader lector;
+
+            try
+            {
+                conexion.ConnectionString = "server=.\\SQLEXPRESS; database = SMG; integrated security = true";
+                comando.CommandType = System.Data.CommandType.Text;
+                comando.CommandText = "select id, name from categories";
+                comando.Connection = conexion;
+
+                conexion.Open();
+                lector = comando.ExecuteReader();
+
+                while (lector.Read())
+                {
+                    Categoria aux = new Categoria();
+                    aux.Id = (int)lector["id"];
+                    aux.Nombre = (string)lector["name"];
+
+                    lista.Add(aux);
+                }
+
+                conexion.Close();
+                return lista;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }          
+        }
+    }
+}
