b24afdc7404bb7fc1810049fc4842db1892760bf
SantiagoMaldonado96 <santy_gm96@hotmail.com>
2021-11-12 00:26:15 -0300

    Alta usuario

--- TPCuatrimestral_Cordoba_Zurita_Maldonado/Coneccion BD/UsuarioNegocio.cs
+++ TPCuatrimestral_Cordoba_Zurita_Maldonado/Coneccion BD/UsuarioNegocio.cs
@@ -3,10 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Mod_Dominio;
 
 namespace Negocio
 {
     public class UsuarioNegocio
     {
+        public void Agregar(Usuario nuevo)
+        {
+            AccesoDatos datos = new AccesoDatos();
+
+            try
+            {
+                datos.SetearConsulta("insert into Users (name, lastName, email, password, cell) values ('" + nuevo.Nombre + "','" + nuevo.Apellido + "','" + nuevo.Email + "','" + nuevo.Contraseña + "','" + nuevo.Celular + "')");
+                datos.EjecutarAccion();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.CerrarConexion();
+            }
+        }
     }
 }
--- TPCuatrimestral_Cordoba_Zurita_Maldonado/TPCuatrimestral_Cordoba_Zurita_Maldonado/Registrarse.aspx.cs
+++ TPCuatrimestral_Cordoba_Zurita_Maldonado/TPCuatrimestral_Cordoba_Zurita_Maldonado/Registrarse.aspx.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Mod_Dominio;
+using Negocio;
 
 namespace TPCuatrimestral_Cordoba_Zurita_Maldonado
 {
@@ -12,6 +14,29 @@ namespace TPCuatrimestral_Cordoba_Zurita_Maldonado
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        protected void btnCrearCuenta_Click(object sender, EventArgs e)
+        {
+            Usuario user = new Usuario();
+            UsuarioNegocio userNeg = new UsuarioNegocio();
+
+            try
+            {
+                user.Nombre = txtNombre.Text;
+                user.Apellido = txtApellido.Text;
+                user.Contraseña = txtContraseña.Text;
+                user.Email = txtEmail.Text;
+                user.Celular = txtCelular.Text;
+
+                userNeg.Agregar(user);
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
         }
     }
 }
