cc293091887920e2c3082a4d5143b29123edcea8
SantiagoMaldonado96 <santy_gm96@hotmail.com>
2021-11-13 19:52:22 -0300

    Verificacion en el Login de Usuario/Administrador

---
+++ TPCuatrimestral_Cordoba_Zurita_Maldonado/TPCuatrimestral_Cordoba_Zurita_Maldonado/OpcionAdmin.aspx.designer.cs
@@ -0,0 +1,44 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace TPCuatrimestral_Cordoba_Zurita_Maldonado
+{
+
+
+    public partial class OpcionAdmin
+    {
+
+        /// <summary>
+        /// Control lblMensaje.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblMensaje;
+
+        /// <summary>
+        /// Control btnCliente.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnCliente;
+
+        /// <summary>
+        /// Control btnAdministrador.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnAdministrador;
+    }
+}
